4559189ec5983d836075ecc8aded4b8a5fbcaf3d
Matteo Pio Napolitano <matteopionapolitano@MacBook-Pro-di-Matteo.local>
2017-03-15 18:51:33 +0100

    Database helper instance moved outside of the cron callback. This ensure that just one instance is alive. Cron callback produces a child process

--- services/cronSvc.js
+++ services/cronSvc.js
@@ -3,11 +3,14 @@ var queueHelper = require('../helper/queueHelper');
 var dbHelper = require('../helper/dbHelper');
 
 var cronSvc = function () {
+    /** local instance of database connection helper */
+    var localDbHelper = dbHelper.getInstance();
+
     this.start = function () {
         cron.schedule('*/2 * * * * *', function () {
             console.log('running a task every 2 seconds');
 
-            dbHelper.getInstance().getMessagesToDispatch(function (results) {
+            localDbHelper.getMessagesToDispatch(function (results) {
                 if (results === null || results.length === 0) {
                     console.log('No messages to dispatch');
                     return;
